a54559f07c9b1abdf1d61cfa5f66704c71f75899
eubeco <36170053+eubeco@users.noreply.github.com>
2019-11-20 11:17:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio2_formulario_dos_idiomas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ejercicio2_formulario_dos_idiomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
